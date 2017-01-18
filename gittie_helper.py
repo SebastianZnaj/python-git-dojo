9c68aac456ab36a45c291c02b0f8b9fe085f2a97
SebastianZnaj <seb.znaj@gmail.com>
2017-01-18 09:42:29 +0100

    first two method added

--- gittie_helper.py
+++ gittie_helper.py
@@ -1,24 +1,25 @@
-class GittieHelper():
+class GittieHelper:
 
     def __init__(self):
         """
         Initialize attributes with default value
         """
-        pass
+        self.temperature_degree = None
+        self.humidity_value = None
 
     def set_temperature(self, temperature_degree):
         """
         Method sets temperature to attribute and validate input
         :param temperature_degree:
         """
-        pass
+        return temperature_degree
 
     def set_humidity(self, humidity_value):
         """
         Method sets humidity level to attribute and validate input
         :param humidity_value:
         """
-        pass
+        return humidity_value
 
     def set_air_pollution(self, air_pollution_level):
         """
@@ -37,6 +38,6 @@ class GittieHelper():
     def get_value(self):
         """
         Method should calculate if exiting home is safe for gittie
-        :param day_number:
+        :param:
         """
         pass
